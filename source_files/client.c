edf5a1b43ecb998ed46a0605663c225c0ab76bc8
Dan Sylvain <dsylvain@c3r1p3.42perpignan.fr>
2023-12-16 13:33:20 +0100

    end session 42

--- source_files/client.c
+++ source_files/client.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 08:45:49 by dsylvain          #+#    #+#             */
-/*   Updated: 2023/12/16 13:11:02 by dsylvain         ###   ########.fr       */
+/*   Updated: 2023/12/16 13:22:18 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,12 +71,31 @@ void	transmit_string_buff(char string_buff[], pid_t server_pid)
 	transmit_string_length(string_buff, server_pid);
 }
 
+int	transmission_loop(char string_buff[], char **input_string, pid_t server_pid)
+{
+	int					bytes_read;
+
+	while (1)
+	{
+		if (string_buff[0])
+			transmit_string_buff(string_buff, server_pid);
+		bytes_read = read(0, string_buff, 5000);
+		if (bytes_read == -1)
+			return (0);
+		else if (bytes_read == 0 || !string_buff[0])
+			return (0);
+		else
+			string_buff[bytes_read] = '\0';
+		input_string = NULL;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	pid_t				server_pid;
 	char				*input_string;
 	char				string_buff[5000];
-	int					bytes_read;
 	struct sigaction	sa_1;
 	struct sigaction	sa_2;
 
@@ -87,18 +106,7 @@ int	main(int argc, char **argv)
 		return (display_error(), 255);
 	ft_printf("Server PID: %i\n", server_pid);
 	sleep(1);
-	while (1)
-	{
-		if (string_buff[0])
-			transmit_string_buff(string_buff, server_pid);
-		bytes_read = read(0, string_buff, 5000);
-		if (bytes_read == -1)
-			return (display_error(), 255);
-		else if (bytes_read == 0 || !string_buff[0])
-			return (0);
-		else
-			string_buff[bytes_read] = '\0';
-		input_string = NULL;
-	}
+	if (!transmission_loop(string_buff, &input_string, server_pid))
+		return (255);
 	return (0);
 }
